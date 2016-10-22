e26def01393601bd2e15d44b340a7db155ed95e2
Mark Miyashita <negativetwelve@gmail.com>
2016-10-22 03:08:42 -0700

    Add methods to start / stop and set user information on the iOS side

--- ios/RNUXCam.h
+++ ios/RNUXCam.h
@@ -5,3 +5,10 @@
 //  Created by Mark Miyashita on 10/22/16.
 //  Copyright © 2016 Mark Miyashita. All rights reserved.
 //
+
+
+#import "RCTBridgeModule.h"
+
+
+@interface RNUXCam : NSObject <RCTBridgeModule>
+@end
--- ios/RNUXCam.m
+++ ios/RNUXCam.m
@@ -5,3 +5,34 @@
 //  Created by Mark Miyashita on 10/22/16.
 //  Copyright © 2016 Mark Miyashita. All rights reserved.
 //
+
+
+#import "RNUXCam.h"
+#import <UXCam/UXCam.h>
+
+
+@implementation RNUXCam
+
+RCT_EXPORT_MODULE();
+
+- (dispatch_queue_t)methodQueue {
+  return dispatch_get_main_queue();
+}
+
+RCT_EXPORT_METHOD(startWithKey:(NSString *)key) {
+  [UXCam startWithKey:key];
+}
+
+RCT_EXPORT_METHOD(stopApplicationAndUploadData) {
+  [UXCam stopApplicationAndUploadData];
+}
+
+RCT_EXPORT_METHOD(tagScreenName:(NSString *)screenName) {
+  [UXCam tagScreenName:screenName];
+}
+
+RCT_EXPORT_METHOD(tagUserName:(NSString *)userName) {
+  [UXCam tagUsersName:userName];
+}
+
+@end
