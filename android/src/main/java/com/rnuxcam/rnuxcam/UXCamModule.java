6ed0f702fa3d01b1a39fb2106c52911909cf542d
Mark Miyashita <negativetwelve@gmail.com>
2017-07-20 06:40:36 -0700

    Remove unused toString call on the UXCamModule for android

--- android/src/main/java/com/rnuxcam/rnuxcam/UXCamModule.java
+++ android/src/main/java/com/rnuxcam/rnuxcam/UXCamModule.java
@@ -63,7 +63,7 @@ public class UXCamModule extends ReactContextBaseJavaModule {
 
     ReadableMapKeySetIterator iterator = properties.keySetIterator();
     while (iterator.hasNextKey()) {
-      String key = iterator.nextKey().toString();
+      String key = iterator.nextKey();
       String value = properties.getString(key);
       map.put(key, value);
     }
