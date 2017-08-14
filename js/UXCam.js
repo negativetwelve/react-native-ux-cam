0c03894aae3a35fc09557170a11c2d1f41b316ac
Mark Miyashita <negativetwelve@gmail.com>
2017-08-14 13:27:58 -0700

    Enable android for UXCam

--- js/UXCam.js
+++ js/UXCam.js
@@ -18,6 +18,7 @@ export default Package.create({
   json: require('../package.json'),
   nativeModule: NativeModules.RNUXCam,
   enabled: Platform.select({
+    android: true,
     ios: true,
   }),
   export: (UXCam) => ({
