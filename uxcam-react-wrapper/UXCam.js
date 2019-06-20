564a1d5209f8aba0783b02e2f6f45fb7dfe6f0d2
Bishwash <bishwash@uxcam.com>
2019-06-20 12:16:03 +0545

    occludeAllTextField bug fixed and Android UXCam lib updated to 3.1.1

--- uxcam-react-wrapper/UXCam.js
+++ uxcam-react-wrapper/UXCam.js
@@ -77,7 +77,7 @@ class UXCam {
         @parameter occludeAll Set YES to hide all UITextField views on the screen in the recording, NO to stop occluding them from the screen recording.
      */
     static occludeAllTextFields(occludeAll) {
-        UXCamBridge.occludeAllTextFields(occlude);
+        UXCamBridge.occludeAllTextFields(occludeAll);
     }
 
     /**
