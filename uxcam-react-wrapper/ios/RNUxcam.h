05ab2e0c9d7c9232ec76eba423e0b64e0f6ea0a3
Richard Groves <richard@noodlfroot.com>
2019-10-04 12:39:17 +0100

    Changing to send events for the verify listener
    Code changes in App.js needed:
    
    import {NativeModules, NativeEventEmitter} from ‘react-native’;
    const RNUxcam = NativeModules.RNUxcam;
    const RNUXCamVerifyEmitter = new NativeEventEmitter(RNUxcam);
    var eventSubscription = RNUXCamVerifyEmitter.addListener(
                                                              ‘UXCam_Verification_Event’,
                                                              (verifyResult) => console.log(“UXCam verify result: ” + verifyResult.success)
                                                            );
    
    RNUxcam.optIntoSchematicRecordings();
    RNUxcam.startWithKey(“YOUR API KEY HERE”);

--- uxcam-react-wrapper/ios/RNUxcam.h
+++ uxcam-react-wrapper/ios/RNUxcam.h
@@ -12,7 +12,8 @@
 
 #import <React/RCTBridgeModule.h>
 #import <React/RCTUIManager.h>
+
 @interface RNUxcam : NSObject <RCTBridgeModule>
 
 @end
-  
+
--- uxcam-react-wrapper/ios/RNUxcam.m
+++ uxcam-react-wrapper/ios/RNUxcam.m
@@ -1,259 +1,271 @@
 #import "RNUxcam.h"
 #import "UXCam.h"
 
-static NSString* const Notification_RNUxcamVerifyCompleted = @"RNUxcamVerifyCompleted";
-static NSString* const RNUxcam_VerifyResult_Key = @"RNUxcam_VerifyResult";
+#import <React/RCTUIManager.h>
+
+static NSString* const RNUxcam_VerifyEvent_Name = @"UXCam_Verification_Event";
 
 @interface RNUxcam ()
 @property (atomic, strong) NSNumber* lastVerifyResult;
-@property (nonatomic, strong) id<NSObject> verifyListener;
+@property (atomic, assign) NSInteger numEventListeners;
 @end
 
 @implementation RNUxcam
 
-@synthesize bridge = _bridge;
 RCT_EXPORT_MODULE();
 
+/// Made the module a singleton when we added event sending - otherwise it threw an error on CMD+R reload
++ (id)allocWithZone:(NSZone *)zone
+{
+	static RNUxcam *sharedInstance = nil;
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		sharedInstance = [super allocWithZone:zone];
+	});
+	return sharedInstance;
+}
+
+/// TODO: Investigate if we can remove this and run on a general background Q
 - (dispatch_queue_t)methodQueue
 {
 	return dispatch_get_main_queue();
 }
 
+#pragma mark The main entry point - start the UXCam system with the provided key
 RCT_EXPORT_METHOD(startWithKey:(NSString *)userAPIKey)
 {
 	self.lastVerifyResult = nil;
 	[UXCam pluginType:@"react-native" version:@"5.1.11"];
+	/// TODO: Move this into the main iOS SDK so it sends a notification on verify and we just listen for that.
 	[UXCam startWithKey:userAPIKey
 		buildIdentifier:nil
 		completionBlock:^(BOOL started)
 						 {
 							 self.lastVerifyResult = @(started);
-							 [NSNotificationCenter.defaultCenter postNotificationName:Notification_RNUxcamVerifyCompleted
-																			   object:self
-																			 userInfo:@{RNUxcam_VerifyResult_Key : self.lastVerifyResult}];
+							 [self verifyEventSender:started];
 						 }
 	 ];
 }
 
-RCT_EXPORT_METHOD(addVerificationListener:(RCTPromiseResolveBlock)resolve
-				  rejecter:(RCTPromiseRejectBlock)reject)
+#pragma mark Event related methods
+- (NSArray<NSString *> *)supportedEvents
 {
-	
-	void (^sendResultBlock)(BOOL result) = ^void(BOOL result)
-											{
-												if (result)
-												{
-													resolve(@"success");
-												}
-												else
-												{
-													NSString *code = @"failed";
-													NSString *message = @"UXCam session verification failed";
-													NSError *error = [NSError errorWithDomain:@"RNUXCam" code:1 userInfo:nil];
-													
-													reject(code, message, error);
-												}
-											};
-	
-	if (self.lastVerifyResult)
+	return @[RNUxcam_VerifyEvent_Name];
+}
+
+/// Will be called when this module's first listener is added.
+-(void)startObserving
+{
+	// Set up any upstream listeners or background tasks as necessary
+	if (self.numEventListeners == 0)
 	{
-		sendResultBlock(self.lastVerifyResult.boolValue);
-		self.lastVerifyResult = nil;
+		/// Action for first listener added - eg register for notifications
 	}
-	else
+	
+	self.numEventListeners++;
+}
+
+/// Will be called when this module's last listener is removed, or on dealloc.
+-(void)stopObserving
+{
+	self.numEventListeners--;
+	if (self.numEventListeners == 0)
 	{
-		NSNotificationCenter * __weak center = NSNotificationCenter.defaultCenter;
-		if (self.verifyListener)
-		{
-			[center removeObserver:self.verifyListener];
-		}
-		
-		self.verifyListener = [center addObserverForName:Notification_RNUxcamVerifyCompleted
-												  object:self
-												   queue:[NSOperationQueue mainQueue]
-											  usingBlock:^(NSNotification *note)
-														   {
-															   if (self.lastVerifyResult)
-															   {
-																   sendResultBlock(self.lastVerifyResult.boolValue);
-																   self.lastVerifyResult = nil;
-															   }
-															   
-															   [center removeObserver:self.verifyListener];
-														   }
-							   ];
+		/// Remove upstream listeners, stop unnecessary background tasks
+	}
+	else if (self.numEventListeners < 0)
+	{
+		NSLog(@"RNUxcam: Removed more event listeners than were added.");
 	}
 }
 
+- (void)verifyEventSender:(BOOL)verifyResult
+{
+	if (self.numEventListeners > 0)
+	{
+		NSDictionary* eventBody = @{@"success": @(verifyResult)};
+		if (verifyResult == FALSE)
+		{
+			NSString *message = @"UXCam session verification failed"; /// TODO: Localise
+			NSError *error = [NSError errorWithDomain:@"RNUXCam" code:1 userInfo:@{NSLocalizedDescriptionKey : message}];
+			
+			eventBody = @{@"success": @(verifyResult), @"error": error};
+		}
+		
+		[self sendEventWithName:RNUxcam_VerifyEvent_Name body:eventBody];
+	}
+}
+
+#pragma mark General UXCam SDK method mappings
 RCT_EXPORT_METHOD(stopSessionAndUploadData)
 {
-    [UXCam stopSessionAndUploadData];
+	[UXCam stopSessionAndUploadData];
 }
 
 RCT_EXPORT_METHOD(restartSession)
 {
-    [UXCam startNewSession];
+	[UXCam startNewSession];
 }
 
 RCT_EXPORT_METHOD(allowShortBreakForAnotherApp:(BOOL)continueSession)
 {
-    [UXCam allowShortBreakForAnotherApp:continueSession];
+	[UXCam allowShortBreakForAnotherApp:continueSession];
 }
 
 RCT_EXPORT_METHOD(startNewSession)
 {
-    [UXCam startNewSession];
+	[UXCam startNewSession];
 }
 
 RCT_EXPORT_METHOD(cancelCurrentSession)
 {
-    [UXCam cancelCurrentSession];
+	[UXCam cancelCurrentSession];
 }
 
 RCT_EXPORT_METHOD(setMultiSessionRecord:(BOOL)recordMultipleSessions)
 {
-    [UXCam setMultiSessionRecord:recordMultipleSessions];
+	[UXCam setMultiSessionRecord:recordMultipleSessions];
 }
 
 RCT_EXPORT_METHOD(pauseScreenRecording)
 {
-    [UXCam pauseScreenRecording];
+	[UXCam pauseScreenRecording];
 }
 
 RCT_EXPORT_METHOD(resumeScreenRecording)
 {
-    [UXCam resumeScreenRecording];
+	[UXCam resumeScreenRecording];
 }
 
 RCT_EXPORT_METHOD(disableCrashHandling:(BOOL)disable)
 {
-    [UXCam disableCrashHandling:disable];
+	[UXCam disableCrashHandling:disable];
 }
 
 RCT_EXPORT_METHOD(occludeSensitiveScreen:(BOOL)hideScreen)
 {
-    [UXCam occludeSensitiveScreen:hideScreen hideGestures:true];
+	[UXCam occludeSensitiveScreen:hideScreen hideGestures:true];
 }
 
 RCT_EXPORT_METHOD(occludeSensitiveScreen:(BOOL)hideScreen hideGestures:(BOOL) hideGesture)
 {
-    [UXCam occludeSensitiveScreen:hideScreen hideGestures:hideGesture];
+	[UXCam occludeSensitiveScreen:hideScreen hideGestures:hideGesture];
 }
 
 RCT_EXPORT_METHOD(occludeAllTextFields:(BOOL)occludeAll)
 {
-    [UXCam occludeAllTextFields:occludeAll];
+	[UXCam occludeAllTextFields:occludeAll];
 }
 
 RCT_EXPORT_METHOD(setUserIdentity:(NSString*)userIdentity)
 {
-    [UXCam setUserIdentity:userIdentity];
+	[UXCam setUserIdentity:userIdentity];
 }
 
 RCT_EXPORT_METHOD(setUserProperty:(NSString*)propertyName value:(id)value)
 {
-    [UXCam setUserProperty:propertyName value:value];
+	[UXCam setUserProperty:propertyName value:value];
 }
 
 RCT_EXPORT_METHOD(setSessionProperty:(NSString*)propertyName value:(id)value)
 {
-    [UXCam setSessionProperty:propertyName value:value];
+	[UXCam setSessionProperty:propertyName value:value];
 }
 
 RCT_EXPORT_METHOD(logEvent:(NSString*)eventName)
 {
-    [UXCam logEvent:eventName];
+	[UXCam logEvent:eventName];
 }
 
 RCT_EXPORT_METHOD(logEvent:(NSString*)eventName withProperties:(nullable NSDictionary<NSString*, id>*)properties)
 {
-    [UXCam logEvent:eventName withProperties:properties];
+	[UXCam logEvent:eventName withProperties:properties];
 }
 
 RCT_EXPORT_METHOD(urlForCurrentUser:(RCTPromiseResolveBlock)resolve
 						   rejecter:(RCTPromiseRejectBlock)reject)
 {
-    NSString *url = [UXCam urlForCurrentUser];
-    
-    if (url)
+	NSString *url = [UXCam urlForCurrentUser];
+	
+	if (url)
 	{
-        resolve(url);
-    }
+		resolve(url);
+	}
 	else
 	{
-        NSString *code = @"no_url";
-        NSString *message = @"Could not retrieve the url for the current user.";
-        NSError *error = [NSError errorWithDomain:@"RNUXCam" code:1 userInfo:nil];
-        
-        reject(code, message, error);
-    }
+		NSString *code = @"no_url";
+		NSString *message = @"Could not retrieve the url for the current user.";
+		NSError *error = [NSError errorWithDomain:@"RNUXCam" code:1 userInfo:nil];
+		
+		reject(code, message, error);
+	}
 }
 
 RCT_EXPORT_METHOD(urlForCurrentSession:(RCTPromiseResolveBlock)resolve
-							  rejecter:(RCTPromiseRejectBlock)reject)
+				  rejecter:(RCTPromiseRejectBlock)reject)
 {
-    NSString *url = [UXCam urlForCurrentSession];
-    
+	NSString *url = [UXCam urlForCurrentSession];
+	
 	if (url)
 	{
-        resolve(url);
-    }
+		resolve(url);
+	}
 	else
 	{
-        NSString *code = @"no_url";
-        NSString *message = @"Could not retrieve the url for the current session.";
-        NSError *error = [NSError errorWithDomain:@"RNUXCam" code:2 userInfo:nil];
-        
-        reject(code, message, error);
-    }
+		NSString *code = @"no_url";
+		NSString *message = @"Could not retrieve the url for the current session.";
+		NSError *error = [NSError errorWithDomain:@"RNUXCam" code:2 userInfo:nil];
+		
+		reject(code, message, error);
+	}
 }
 
 RCT_EXPORT_METHOD(optOutOverall)
 {
-    [UXCam optOutOverall];
+	[UXCam optOutOverall];
 }
 
 RCT_EXPORT_METHOD(optOutOfSchematicRecordings)
 {
-    [UXCam optOutOfSchematicRecordings];
+	[UXCam optOutOfSchematicRecordings];
 }
 
 RCT_EXPORT_METHOD(optInOverall)
 {
-    [UXCam optInOverall];
+	[UXCam optInOverall];
 }
 
 RCT_EXPORT_METHOD(optIntoSchematicRecordings)
 {
-    [UXCam optIntoSchematicRecordings];
+	[UXCam optIntoSchematicRecordings];
 }
 
 RCT_EXPORT_METHOD(optInOverallStatus:(RCTPromiseResolveBlock)resolve
-                  			rejecter:(RCTPromiseRejectBlock)reject)
+				  			rejecter:(RCTPromiseRejectBlock)reject)
 {
 	resolve(@(UXCam.optInOverallStatus));
 }
 
 RCT_EXPORT_METHOD(optInSchematicRecordingStatus:(RCTPromiseResolveBlock)resolve
-                  					   rejecter:(RCTPromiseRejectBlock)reject)
+				  					   rejecter:(RCTPromiseRejectBlock)reject)
 {
 	resolve(@(UXCam.optInSchematicRecordingStatus));
 }
 
 RCT_EXPORT_METHOD(isRecording:(RCTPromiseResolveBlock)resolve
-					rejecter:(RCTPromiseRejectBlock)reject)
+				  	 rejecter:(RCTPromiseRejectBlock)reject)
 {
 	resolve(@(UXCam.isRecording));
 }
 
 RCT_EXPORT_METHOD(getMultiSessionRecord:(RCTPromiseResolveBlock)resolve
-                  			   rejecter:(RCTPromiseRejectBlock)reject)
+							   rejecter:(RCTPromiseRejectBlock)reject)
 {
-    resolve(@(UXCam.getMultiSessionRecord));
+	resolve(@(UXCam.getMultiSessionRecord));
 }
 
 RCT_EXPORT_METHOD(pendingSessionCount:(RCTPromiseResolveBlock)resolve
-                  			 rejecter:(RCTPromiseRejectBlock)reject)
+							 rejecter:(RCTPromiseRejectBlock)reject)
 {
 	resolve(@(UXCam.pendingUploads));
 }
@@ -270,29 +282,29 @@ RCT_EXPORT_METHOD(resumeShortBreakForAnotherApp)
 
 RCT_EXPORT_METHOD(occludeSensitiveView: (nonnull NSNumber *) tag)
 {
-    dispatch_async(dispatch_get_main_queue(), ^
-	{
-        UIView* view = [self.bridge.uiManager viewForReactTag:tag];
-        [UXCam occludeSensitiveView:view];
-    });
+	dispatch_async(dispatch_get_main_queue(), ^
+				   {
+					   UIView* view = [self.bridge.uiManager viewForReactTag:tag];
+					   [UXCam occludeSensitiveView:view];
+				   });
 }
 
 RCT_EXPORT_METHOD(unOccludeSensitiveView: (nonnull NSNumber *) tag)
 {
-    dispatch_async(dispatch_get_main_queue(), ^
-	{
-        UIView* view = [self.bridge.uiManager viewForReactTag:tag];
-        [UXCam unOccludeSensitiveView:view];
-    });
+	dispatch_async(dispatch_get_main_queue(), ^
+				   {
+					   UIView* view = [self.bridge.uiManager viewForReactTag:tag];
+					   [UXCam unOccludeSensitiveView:view];
+				   });
 }
 
 RCT_EXPORT_METHOD(occludeSensitiveViewWithoutGesture: (nonnull NSNumber *) tag)
 {
-    dispatch_async(dispatch_get_main_queue(), ^
-	{
-        UIView* view = [self.bridge.uiManager viewForReactTag:tag];
-        [UXCam occludeSensitiveViewWithoutGesture:view];
-    });
+	dispatch_async(dispatch_get_main_queue(), ^
+				   {
+					   UIView* view = [self.bridge.uiManager viewForReactTag:tag];
+					   [UXCam occludeSensitiveViewWithoutGesture:view];
+				   });
 }
 
 #pragma mark Screen name methods
@@ -332,7 +344,7 @@ RCT_EXPORT_METHOD(removeAllScreenNamesToIgnore)
 }
 
 RCT_EXPORT_METHOD(screenNamesBeingIgnored:(RCTPromiseResolveBlock)resolve
-				  				 rejecter:(RCTPromiseRejectBlock)reject)
+				  				rejecter:(RCTPromiseRejectBlock)reject)
 {
 	resolve(UXCam.screenNamesBeingIgnored);
 }
